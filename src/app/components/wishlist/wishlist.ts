ecaa1a613735221e798b47721a0a063a9b253274
minasameh11 <minasameh492@gmail.com>
2025-06-19 00:04:04 +0200

    Edit wishlist ui and logic

--- src/app/components/wishlist/wishlist.ts
+++ src/app/components/wishlist/wishlist.ts
@@ -1,19 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../shared/movie-service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [CommonModule, FormsModule],
+  imports: [RouterLink, RouterLinkActive,CommonModule, FormsModule],
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.css'
 })
-export class Wishlist {
+export class Wishlist  {
+  
+  private favoritesSubject = new BehaviorSubject<any[]>([]);
+  favorites: any[] = []
+  counter: number = 0;
 
-  movies: any[] = []
+
   constructor(private _movie : MovieService){
-     this.movies = this._movie.favorites;
+     this.favorites = this._movie.favorites;
   }
 
+
+ getFavorites(): any[] {
+    return this.favorites;
+  }
+
+  addFavorite(movie: any): void {
+    if (!this.isFavorite(movie)) {
+      this.favorites.push(movie);
+      this.favoritesSubject.next(this.favorites);
+    }
+  }
+
+  removeFavorite(movie: any): void {
+    this.favorites = this.favorites.filter(m => m.id !== movie.id);
+    this.favoritesSubject.next(this.favorites);
+  }
+
+  toggleFavorite(movie: any): void {
+    const index = this.favorites.findIndex(f => f.id === movie.id);
+  if (index > -1) {
+    this.favorites.splice(index, 1); 
+    this._movie.decreaseCounter();
+  } else {
+    this.favorites.push(movie); 
+    this._movie.increaseCounter();
+  }
+  this._movie.favorites = this.favorites;
+  }
+
+  isFavorite(movie: any): boolean {
+    return this.favorites.some(m => m.id === movie.id);
+  }
+
+  getCount(): number {
+    return this.favorites.length;
+  }
+
+  
 }
